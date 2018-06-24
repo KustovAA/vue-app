9492ddb88ef61afacb91523c6bbd7fcdfb305d0d
Andriy Ivashchenko <andr.ivas12@gmail.com>
2018-06-24 19:37:13 +0300

    first

---
+++ src/entry-server.js
@@ -0,0 +1,27 @@
+import { createApp } from './main.js';
+
+export default context => {
+    // since there could potentially be asynchronous route hooks or components,
+    // we will be returning a Promise so that the server can wait until
+    // everything is ready before rendering.
+    return new Promise((resolve, reject) => {
+        const { app, router } = createApp();
+
+        // set server-side router's location
+        console.log(context.url);
+        router.push(context.url);
+        
+        // wait until router has resolved possible async components and hooks
+        router.onReady(() => {
+            const matchedComponents = router.getMatchedComponents();
+            console.log('asd', matchedComponents)
+            // no matched routes, reject with 404
+            if (!matchedComponents.length) {
+                console.log('err')
+                reject({ code: 404, message: 'Page not found' });
+            }
+            resolve(app);
+            // the Promise should resolve to the app instance so it can be rendered
+        }, reject);
+    });
+}
